b623bda4a6232a1d725f817d825ab760348d1590
Yiheng Su <ysu707@login2.ls6.tacc.utexas.edu>
2022-06-14 18:17:25 -0500

    Small bug fix on preprocess

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@ data_file = sys.argv[1]
 output_file = sys.argv[2]
 
 columns = ["Tweet", "Orig_Label", "Vote"]
-data = pd.read_csv(data_file, sep=None, names=columns)
+data = pd.read_csv(data_file, sep=None, names=columns, engine='python')
 data["Label"] = data["Orig_Label"] == "hateful"
 data.to_csv(output_file, header=True, index=False)
 
