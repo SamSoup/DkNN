9f024aba5eb3cb104018f93f618e30c7b2da9bbd
SamSoup <sam.su@utexas.edu>
2023-05-22 18:10:09 -0500

    add llama to configuration

--- constants.py
+++ constants.py
@@ -12,58 +12,41 @@ sys.path.append(WORK_DIR)
 #     42, 365, 469, 4399, 3012023
 # ]
 
-SEEDS = [
-    42
-]
+SEEDS = [42]
 
-SPLITS = ['train', 'eval', 'test']
+SPLITS = ["train", "eval", "test"]
 
 # dataset -> layers computed
-DATASETS = {
-    'toxigen': ['Last Only'],
-    'esnli': ['Last Only']
-}
+DATASETS = {"toxigen": ["Last Only"], "esnli": ["Last Only"]}
 
-LAYER_CONFIGS = [
-    "All",
-    "Embedding Only",
-    "Embedding + Last",
-    "Last Only"
-]
+LAYER_CONFIGS = ["All", "Embedding Only", "Embedding + Last", "Last Only"]
 
 BASELINES = [
-    'Glove-Twitter-200',
-    'FastText-300',
-    'Google-news-300',
-    'SentenceBert'
+    "Glove-Twitter-200",
+    "FastText-300",
+    "Google-news-300",
+    "SentenceBert",
 ]
 
-METRICS = [
-    "accuracy",
-    "precision",
-    "recall",
-    "f1"
-]
+METRICS = ["accuracy", "precision", "recall", "f1"]
 
 MODELS = [
     "bart-large",
     "deberta-large",
     "flan-t5-large"
     # "t5-large"
+    "llama7B",
 ]
 
-WRAPPER_BOXES = [
-    "KNN",
-    "SVM",
-    "Decision_Tree",
-    "L_Means"
-]
+WRAPPER_BOXES = ["KNN", "SVM", "Decision_Tree", "L_Means"]
 
 # defines where models outputs are stored
 MODEL_CONFIGS = {
     dataset: {
         model: {
-            seed: os.path.join(WORK_DIR, "output", dataset, f"{model}-seed-{seed}")
+            seed: os.path.join(
+                WORK_DIR, "output", dataset, f"{model}-seed-{seed}"
+            )
             for seed in SEEDS
         }
         for model in MODELS
@@ -74,33 +57,37 @@ MODEL_CONFIGS = {
 # defines metadata specific to each model
 MODEL_METADATAS = {
     "bart-large": {
-        'num_layers': 26,
-        'available_poolers': [
+        "num_layers": 26,
+        "available_poolers": [
             "mean_with_attention",
             # "mean_with_attention_and_eos"
-        ]
+        ],
     },
     "deberta-large": {
-        'num_layers': 25,
-        'available_poolers': [
+        "num_layers": 25,
+        "available_poolers": [
             "mean_with_attention",
             # "mean_with_attention_and_cls"
-        ]
+        ],
     },
     "flan-t5-large": {
-        'num_layers': 50,
-        'available_poolers': [
+        "num_layers": 50,
+        "available_poolers": [
             "mean_with_attention",
             # "encoder_mean_with_attention_and_decoder_flatten"
-        ]
+        ],
     },
     "t5-large": {
-        'num_layers': 50,
-        'available_poolers': [
+        "num_layers": 50,
+        "available_poolers": [
             "mean_with_attention",
             # "encoder_mean_with_attention_and_decoder_flatten"
-        ]
-    }
+        ],
+    },
+    "llama7B": {
+        "num_layers": 33,
+        "available_poolers": ["mean_with_attention"],
+    },
 }
 
 DATA = {
@@ -111,13 +98,11 @@ DATA = {
 # dataset name -> split -> labels
 LABELS = {
     dataset: {
-        split: np.array(DATA[dataset][split]['label'])
-        for split in SPLITS
+        split: np.array(DATA[dataset][split]["label"]) for split in SPLITS
     }
     for dataset in DATASETS
 }
 
 DATA_PATH = os.path.join(
-    WORK_DIR,
-    "data/{dataset}/{model}/{mode}/{pooler_config}/layer_{layer}.csv"
+    WORK_DIR, "data/{dataset}/{model}/{mode}/{pooler_config}/layer_{layer}.csv"
 )
--- run_wrappers.py
+++ run_wrappers.py
@@ -7,7 +7,7 @@ from utils import (
     compute_metrics,
     get_actual_layers_to_save,
     find_majority_batched,
-    mkdir_if_not_exists
+    mkdir_if_not_exists,
 )
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
@@ -16,14 +16,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from classifiers.KMeansClassifier import KMeansClassifier
 from classifiers.KMedoidsClassifier import KMedoidsClassifier
 from classifiers.AgglomerativeClusteringClassifier import (
-    AgglomerativeClusteringClassifier
+    AgglomerativeClusteringClassifier,
 )
 from classifiers.LabelBiasedClusteringClassifier import (
-    LabelBiasedClusteringClassifier
+    LabelBiasedClusteringClassifier,
 )
-from classifiers.DecisionTreeProxyClassifier import (
-    DecisionTreeProxyClassifier
-)
+from classifiers.DecisionTreeProxyClassifier import DecisionTreeProxyClassifier
 from sklearn.model_selection import GridSearchCV
 from constants import (
     DATASETS,
@@ -34,7 +32,7 @@ from constants import (
     MODEL_METADATAS,
     WORK_DIR,
     SEEDS,
-    SPLITS
+    SPLITS,
 )
 
 from sklearn import tree
@@ -51,66 +49,95 @@ import sys
 WORK_DIR = "/work/06782/ysu707/ls6/DkNN"
 sys.path.append(WORK_DIR)
 
-# these take several hyperparameters and honestly I do not know how to 
+# these take several hyperparameters and honestly I do not know how to
 # parse all of them from commandline, so I just put some I think are reasonable
 # ones for now
 all_whiteboxes = {
-    'toxigen': {
-        'SVM': (SVC(gamma='auto', class_weight='balanced', kernel="linear", random_state=42), {}),
-        'Decision_Tree': (DecisionTreeClassifier(max_depth=3, min_samples_leaf=10, random_state=42), {}),
-        'KNN': (KNeighborsClassifier(n_neighbors=5, n_jobs=-1), {}),
+    "toxigen": {
+        "SVM": (
+            SVC(
+                gamma="auto",
+                class_weight="balanced",
+                kernel="linear",
+                random_state=42,
+            ),
+            {},
+        ),
+        "Decision_Tree": (
+            DecisionTreeClassifier(
+                max_depth=3, min_samples_leaf=10, random_state=42
+            ),
+            {},
+        ),
+        "KNN": (KNeighborsClassifier(n_neighbors=5, n_jobs=-1), {}),
         # 'Decision Tree Proxy': (DecisionTreeProxyClassifier(max_depth=3), {}),
         # 'Label Biased Hierarchical Clustering': (
         #     LabelBiasedClusteringClassifier(max_depth=3), {}
         # ),
         # 'Agglomerative Hierarchical Clustering': (AgglomerativeClusteringClassifier(
-        #     max_depth=1, linkage="ward", n_clusters=2), {'n_clusters': [2, 3], 
+        #     max_depth=1, linkage="ward", n_clusters=2), {'n_clusters': [2, 3],
         #                                                  'max_depth': [1, 2, 3]}),
-        'L_Means': (KMeansClassifier(n_clusters=2, random_state=42), {}),
+        "L_Means": (KMeansClassifier(n_clusters=2, random_state=42), {}),
         # 'K-medoids': (KMedoidsClassifier(), {'n_clusters': [2, 3, 10, 20, 30]})
     },
-    'esnli': {
-        'Decision_Tree': (DecisionTreeClassifier(max_depth=3, min_samples_leaf=10, random_state=42), {}),
-        'KNN': (KNeighborsClassifier(n_neighbors=5, n_jobs=-1), {}),
+    "esnli": {
+        "Decision_Tree": (
+            DecisionTreeClassifier(
+                max_depth=3, min_samples_leaf=10, random_state=42
+            ),
+            {},
+        ),
+        "KNN": (KNeighborsClassifier(n_neighbors=5, n_jobs=-1), {}),
         # 'Decision Tree Proxy': (DecisionTreeProxyClassifier(max_depth=3), {}),
         # 'Label Biased Hierarchical Clustering': (
         #     LabelBiasedClusteringClassifier(max_depth=3), {}
         # ),
         # 'Agglomerative Hierarchical Clustering': (AgglomerativeClusteringClassifier(
-        #     max_depth=1, linkage="ward", n_clusters=3), {'n_clusters': [2, 3], 
+        #     max_depth=1, linkage="ward", n_clusters=3), {'n_clusters': [2, 3],
         #                                                  'max_depth': [1, 2, 3]}),
-        'L_Means': (KMeansClassifier(n_clusters=3, random_state=42), {}),
-        'SVM': (LinearSVC(tol=1e-4, dual=False, random_state=42), {}),
+        "L_Means": (KMeansClassifier(n_clusters=3, random_state=42), {}),
+        "SVM": (LinearSVC(tol=1e-4, dual=False, random_state=42), {}),
         # 'K-medoids': (KMedoidsClassifier(), {'n_clusters': [2, 3, 10, 20, 30]})
-    }
+    },
 }
 
 results_df = pd.DataFrame(
-    columns = [
-        'Dataset', 'Model', 'Whitebox', 'Layers_Used', 'Poolers_Used', 'Seed',
-        'predict_accuracy', 'predict_f1', 
-        'predict_precision', 'predict_recall',
+    columns=[
+        "Dataset",
+        "Model",
+        "Whitebox",
+        "Layers_Used",
+        "Poolers_Used",
+        "Seed",
+        "predict_accuracy",
+        "predict_f1",
+        "predict_precision",
+        "predict_recall",
     ]
 )
 
-def save_model_metadata(
-    clf, clf_name, model_name, save_path, layer_postfix
-):
+
+def save_model_metadata(clf, clf_name, model_name, save_path, layer_postfix):
     mkdir_if_not_exists(save_path)
     with open(
-        os.path.join(save_path, f'{model_name}_best_{clf_name}_{layer_postfix}'),
-        'wb'
+        os.path.join(
+            save_path, f"{model_name}_best_{clf_name}_{layer_postfix}"
+        ),
+        "wb",
     ) as f:
         pickle.dump(clf, f)
 
-def run_whiteboxes(X_train, X_eval, X_test, y_train, y_eval, whiteboxes, **kwargs):
+
+def run_whiteboxes(
+    X_train, X_eval, X_test, y_train, y_eval, whiteboxes, **kwargs
+):
     """
-    This method runs all white box methods for representations retrieved from 
+    This method runs all white box methods for representations retrieved from
     a particular layer
     """
     preds = {}
     is_multiclass = np.unique(y_train).size > 2
-    scoring = "f1" if not is_multiclass else 'accuracy'
+    scoring = "f1" if not is_multiclass else "accuracy"
     for name, clf_set in tqdm(whiteboxes.items(), desc="whiteboxes"):
         clf, parameters = clf_set
         if parameters:
@@ -122,23 +149,25 @@ def run_whiteboxes(X_train, X_eval, X_test, y_train, y_eval, whiteboxes, **kwarg
         preds[name] = y_pred
     return preds
 
+
 def add_to_dataframe(df, results: Dict[str, float], **kwargs):
     results.update(kwargs)
     # small helper to add to the current dataframe
-    return pd.concat(
-        [df, pd.DataFrame(results, index=[0])], ignore_index = True
-    )
+    return pd.concat([df, pd.DataFrame(results, index=[0])], ignore_index=True)
 
+
+MODELS = ["llama7B"]
+DATASETS = {"toxigen": ["Last Only"]}
 # compute the white box results for the different transformers
 for dataset, layer_configs in tqdm(DATASETS.items(), desc="Datasets"):
     save_whitebox_path = os.path.join(WORK_DIR, "results", dataset)
     model_configs = MODEL_CONFIGS[dataset]
     Y = LABELS[dataset]
-    is_multiclass = np.unique(Y['train']).size > 2
+    is_multiclass = np.unique(Y["train"]).size > 2
     whiteboxes = all_whiteboxes[dataset]
     for model_name in tqdm(MODELS, desc="Models"):
-        num_layers = MODEL_METADATAS[model_name]['num_layers']
-        pooler_configs = MODEL_METADATAS[model_name]['available_poolers']
+        num_layers = MODEL_METADATAS[model_name]["num_layers"]
+        pooler_configs = MODEL_METADATAS[model_name]["available_poolers"]
         for seed in tqdm(SEEDS, desc="seeds"):
             model_name_with_seed = f"{model_name}-seed-{seed}"
             # layer -> {wrapper box: preds}
@@ -146,48 +175,60 @@ for dataset, layer_configs in tqdm(DATASETS.items(), desc="Datasets"):
             # layer_config -> wrapper_name -> predictions_stacked
             layer_to_wrapper_to_preds = {}
             for layer_config in tqdm(layer_configs, desc="Layer Configs"):
-                layers_to_save = get_actual_layers_to_save(layer_config, num_layers)
+                layers_to_save = get_actual_layers_to_save(
+                    layer_config, num_layers
+                )
                 for pooler_config in tqdm(pooler_configs, desc="Poolers"):
                     save_whitebox_path_model = os.path.join(
                         save_whitebox_path, model_name, pooler_config
                     )
                     for layer in tqdm(layers_to_save, desc="layers"):
                         if layer not in all_y_preds:
-                            all_y_preds[layer] = {} # wrapper box -> preds
-                            X  = {
-                                f'X_{split}': np.loadtxt(
+                            all_y_preds[layer] = {}  # wrapper box -> preds
+                            X = {
+                                f"X_{split}": np.loadtxt(
                                     DATA_PATH.format(
                                         dataset=dataset,
                                         model=model_name_with_seed,
                                         mode=split,
                                         pooler_config=pooler_config,
-                                        layer=layer
+                                        layer=layer,
                                     ),
-                                    delimiter=','
-                                ) for split in SPLITS
+                                    delimiter=",",
+                                )
+                                for split in SPLITS
                             }
                             y_preds = run_whiteboxes(
-                                **X, y_train=Y['train'], y_eval=Y['eval'],
+                                **X,
+                                y_train=Y["train"],
+                                y_eval=Y["eval"],
                                 whiteboxes=whiteboxes,
                                 model_name=model_name_with_seed,
-                                save_path=save_whitebox_path_model, 
-                                layer_postfix=f"layer{layer}"
+                                save_path=save_whitebox_path_model,
+                                layer_postfix=f"layer{layer}",
                             )
                             for wrapper in whiteboxes:
                                 all_y_preds[layer][wrapper] = y_preds[wrapper]
-                # for configurations with more than one layers, then each 
-                # inference example has a prediction from each layer. in this 
+                # for configurations with more than one layers, then each
+                # inference example has a prediction from each layer. in this
                 # case, we stack the predictions into (n, num_layers) and retrieve
                 # the majority voted prediction per n
                 layer_to_wrapper_to_preds[layer_config] = {}
                 for wrapper in tqdm(whiteboxes):
-                    y_preds_stacked = np.concatenate([
-                        all_y_preds[l][wrapper].reshape(-1,1) 
-                        for l in layers_to_save
-                    ], axis=1)
-                    layer_to_wrapper_to_preds[layer_config][wrapper] = y_preds_stacked
+                    y_preds_stacked = np.concatenate(
+                        [
+                            all_y_preds[l][wrapper].reshape(-1, 1)
+                            for l in layers_to_save
+                        ],
+                        axis=1,
+                    )
+                    layer_to_wrapper_to_preds[layer_config][
+                        wrapper
+                    ] = y_preds_stacked
                     y_pred = find_majority_batched(y_preds_stacked)
-                    results = compute_metrics(Y['test'], y_pred, 'predict', is_multiclass)
+                    results = compute_metrics(
+                        Y["test"], y_pred, "predict", is_multiclass
+                    )
                     results_df = add_to_dataframe(
                         results_df,
                         results,
@@ -196,12 +237,12 @@ for dataset, layer_configs in tqdm(DATASETS.items(), desc="Datasets"):
                         Whitebox=wrapper,
                         Layers_Used=layer_config,
                         Seed=seed,
-                        Poolers_Used=pooler_config
+                        Poolers_Used=pooler_config,
                     )
                     # also, run stacked predictions combining KNN, SVM, and DT
                     # ensemble = np.hstack([
-                    #     layer_to_wrapper_to_preds[layer_config]['KNN'], 
-                    #     layer_to_wrapper_to_preds[layer_config]['Decision Tree'], 
+                    #     layer_to_wrapper_to_preds[layer_config]['KNN'],
+                    #     layer_to_wrapper_to_preds[layer_config]['Decision Tree'],
                     #     layer_to_wrapper_to_preds[layer_config]['SVM']
                     # ])
                     # y_pred = find_majority_batched(ensemble)
@@ -233,16 +274,16 @@ for dataset, layer_configs in tqdm(DATASETS.items(), desc="Datasets"):
 #         save_whitebox_path = os.path.join(WORK_DIR, "results", dataset)
 #         X = {
 #             f'X_{split}': np.loadtxt(
-#                 os.path.join(data_path, f"{split}_sentence_representations.txt"), 
+#                 os.path.join(data_path, f"{split}_sentence_representations.txt"),
 #                 delimiter=","
 #             )
 #             for split in SPLITS
 #         }
 #         y_preds = run_whiteboxes(
 #             **X, y_train=Y['y_train'], y_eval=Y['y_eval'],
-#             whiteboxes=whiteboxes, 
+#             whiteboxes=whiteboxes,
 #             model_name=baseline,
-#             save_path=save_whitebox_path, 
+#             save_path=save_whitebox_path,
 #             layer_postfix=""
 #         )
 #         for wrapper, y_pred in tqdm(y_preds.items()):
@@ -257,8 +298,8 @@ for dataset, layer_configs in tqdm(DATASETS.items(), desc="Datasets"):
 #                 Poolers_Used=None
 #             )
 #         ensemble = np.hstack([
-#             y_preds['KNN'].reshape(-1, 1), 
-#             y_preds['Decision Tree'].reshape(-1, 1), 
+#             y_preds['KNN'].reshape(-1, 1),
+#             y_preds['Decision Tree'].reshape(-1, 1),
 #             y_preds['SVM'].reshape(-1, 1)
 #         ])
 #         y_pred = find_majority_batched(ensemble)
@@ -274,6 +315,8 @@ for dataset, layer_configs in tqdm(DATASETS.items(), desc="Datasets"):
 #         )
 output_filename = sys.argv[1]
 results_df.to_csv(
-    path_or_buf=os.path.join(WORK_DIR, output_filename), 
-    index=False, index_label=False, header=True
+    path_or_buf=os.path.join(WORK_DIR, output_filename),
+    index=False,
+    index_label=False,
+    header=True,
 )
