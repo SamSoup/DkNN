173fd9429634e2a7b47d2496e9c49bccd96e7287
SamSoup <sam.su@utexas.edu>
2023-05-30 20:43:22 -0500

    compute test for all datasets

--- compute_test_set_results.py
+++ compute_test_set_results.py
@@ -1,6 +1,14 @@
 from tqdm.auto import tqdm
 from datasets import load_dataset
-from constants import DATASETS, MODELS, WORK_DIR, SEEDS, METRICS, MODEL_METADATAS, WRAPPER_BOXES
+from constants import (
+    DATASETS,
+    MODELS,
+    WORK_DIR,
+    SEEDS,
+    METRICS,
+    MODEL_METADATAS,
+    WRAPPER_BOXES,
+)
 from utils import load_predictions, compute_metrics
 import pandas as pd
 import numpy as np
@@ -10,7 +18,6 @@ import os
 SEEDS = list(filter(lambda x: x == 42, SEEDS))
 pooler_config = "mean_with_attention"
 
-DATASETS = ['toxigen'] # TODO: remove
 
 def create_result_df(models, metrics, classifiers):
     """
@@ -18,36 +25,45 @@ def create_result_df(models, metrics, classifiers):
     and a multilevel column index of models * metrics
     """
     return pd.DataFrame(
-        np.nan, index=classifiers, 
-        columns=pd.MultiIndex.from_product([models, metrics], names=['models', 'metrics'])
+        np.nan,
+        index=classifiers,
+        columns=pd.MultiIndex.from_product(
+            [models, metrics], names=["models", "metrics"]
+        ),
     )
 
-classifiers = ['original'] + WRAPPER_BOXES
+
+classifiers = ["original"] + WRAPPER_BOXES
 for dataset in tqdm(DATASETS, desc="datasets"):
     data = load_dataset(f"Samsoup/{dataset}", use_auth_token=True)
-    y_test = np.array(data['test']['label'])
+    y_test = np.array(data["test"]["label"])
     is_multiclass = np.unique(y_test).size > 2
     whitebox_preds = pd.read_pickle(
-        os.path.join(WORK_DIR, 'data', dataset, f'{dataset}_wrapper_box_predictions.pkl')
+        os.path.join(
+            WORK_DIR, "data", dataset, f"{dataset}_wrapper_box_predictions.pkl"
+        )
     )
-    # result file layout: 
+    # result file layout:
     results = create_result_df(MODELS, METRICS, classifiers)
     for model in tqdm(MODELS, desc="models"):
         for seed in SEEDS:
             model_full = f"{model}-seed-{seed}"
-            layer = MODEL_METADATAS[model]['num_layers']-1
+            layer = MODEL_METADATAS[model]["num_layers"] - 1
             # for each metric, compute metrics for the model to each wrapper box
             for clf in tqdm(classifiers, desc="original+whiteboxes"):
                 if clf == "original":
-                    preds = np.array(load_predictions(WORK_DIR, dataset, model_full))
+                    preds = np.array(
+                        load_predictions(WORK_DIR, dataset, model_full)
+                    )
                 else:
                     preds = whitebox_preds.loc[model][clf]
                 metrics = compute_metrics(
-                    y_test, preds,
-                    prefix="test", is_multiclass=is_multiclass
+                    y_test, preds, prefix="test", is_multiclass=is_multiclass
                 )
                 for metric in METRICS:
-                    results.loc[clf][model][metric] = metrics[f'test_{metric}']
+                    results.loc[clf][model][metric] = metrics[f"test_{metric}"]
     results.to_pickle(
-        os.path.join(WORK_DIR, 'data', dataset, f"{dataset}_test_set_results.pkl")
+        os.path.join(
+            WORK_DIR, "data", dataset, f"{dataset}_test_set_results.pkl"
+        )
     )
