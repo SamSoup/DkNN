9d4260bec899eebf4717c9497187efeb2a057c9a
SamSoup <sam.su@utexas.edu>
2023-06-06 00:26:24 -0500

    try a different prompt

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -9,14 +9,11 @@ import numpy as np
 import pickle
 
 PROMPT_DICT = {
-    "prompt_cls": (
-        "Generate a classification label indicating if the following text is"
-        " {categories}.\n\nText:\n{text}\n\nLabel:\n"
-    ),
+    "prompt_cls": "Classify the given text as {categories}.Text:{text}Label:",
     "prompt_nli": (
         "Given the premise and hypothesis, classify the relationship as one of"
         " entailment, neutral, or contradiction.\n\nPremise:\n{premise}\n\n"
-        " Hypothesis:\n{hypothesis}\n\nRelation:\n"
+        " Hypothesis:\n{hypothesis}\n\nRelation:"
     ),
 }
 
