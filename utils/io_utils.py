e4066da548f1d6fb89a908d67bdf3beb59a77baf
SamSoup <sam.su@utexas.edu>
2023-06-05 19:17:39 -0500

    label tokenization

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -11,12 +11,12 @@ import pickle
 PROMPT_DICT = {
     "prompt_cls": (
         "Generate a classification label indicating if the following text is"
-        " {categories}.\n\nText:\n{text}\n\nLabel:"
+        " {categories}.\n\nText:\n{text}\n\nLabel:\n"
     ),
     "prompt_nli": (
         "Given the premise and hypothesis, classify the relationship as one of"
         " entailment, neutral, or contradiction.\n\nPremise:\n{premise}\n\n"
-        " Hypothesis:\n{hypothesis}\n\nRelation:"
+        " Hypothesis:\n{hypothesis}\n\nRelation:\n"
     ),
 }
 
